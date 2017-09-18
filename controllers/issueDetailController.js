c18e2bbfbbaff7b1a66d182461b7d38cc54c4138
Nishant Shekhar <coderundebug12@gmail.com>
2017-09-18 23:13:53 +0530

    123456

--- controllers/issueDetailController.js
+++ controllers/issueDetailController.js
@@ -1,5 +1,49 @@
 app.controller('issueDetailController',function($scope,$route,$filter){
-    $scope.issuenumber = $route.current.params.id;
+    $scope.issueNumber = $route.current.params.id;
     $scope.issueList = JSON.parse(window.localStorage.getItem('issues'));
-    $scope.currentIssue = $filter('filter')($scope.issueList, {issueNumber:$scope.issueNumber}, true, {})
+    $scope.currentIssue = $filter('filter')($scope.issueList, { issueNumber : $scope.issueNumber})[0];
+    var author = ['Nishant', 'Ravi', 'Nikesh', 'CodeRunDebug', 'Sam', 'Larry']
+    var projectversion = ['Bootstrap', 'BootstrapV3', 'BootstrapV4']
+    var arr = JSON.parse(window.localStorage.getItem('issues')) == null ? [] : JSON.parse(window.localStorage.getItem('issues'));
+    console.log(JSON.parse(window.localStorage.getItem('issues')))
+    var obj = {
+        issueNumber: Math.floor(Math.random() * 1000) + 1,
+        issueName: '',
+        author: '',
+        label: '',
+        projectversion: '',
+        milestones: 'Milestone1',
+        assignedTo: '',
+        sortBy: '',
+        status: '',
+        comments: [],
+        description: '',
+        modifiedDateTime: new Date()
+    }
+    $scope.tinymceOptions = {
+        height: 200,
+        branding: false,
+        statusbar: false,
+        skin: 'lightgray',
+        theme: 'modern',
+        menu: {},
+        init_instance_callback: function (editor) {
+            var textContentTrigger = function () {
+                $scope.description = editor.getBody().textContent;
+                $scope.$apply();
+            };
+            editor.on('KeyUp', textContentTrigger);
+        }
+    }
+    $scope.submit = function (data) {
+        var rand = Math.floor(Math.random() * 6) + 1;
+        obj.issueName = $scope.title;
+        obj.description = $scope.description;
+        obj.author = author[rand];
+        obj.assignedTo = author[rand];
+        obj.status = 'Open';
+        arr.push(obj);
+        window.localStorage.setItem('issues',JSON.stringify(arr))
+        window.location = '#!/issuedetail/' + obj.issueNumber;
+    }
 })
--- controllers/newIssuesController.js
+++ controllers/newIssuesController.js
@@ -1,7 +1,9 @@
 app.controller('newIssues', function ($scope) {
+    $scope.tiny = '';
     var author = ['Nishant', 'Ravi', 'Nikesh', 'CodeRunDebug', 'Sam', 'Larry']
     var projectversion = ['Bootstrap', 'BootstrapV3', 'BootstrapV4']
-    var arr = [];
+    var arr = JSON.parse(window.localStorage.getItem('issues')) == null ? [] : JSON.parse(window.localStorage.getItem('issues'));
+    console.log(JSON.parse(window.localStorage.getItem('issues')))
     var obj = {
         issueNumber: Math.floor(Math.random() * 1000) + 1,
         issueName: '',
@@ -16,15 +18,20 @@ app.controller('newIssues', function ($scope) {
         description: '',
         modifiedDateTime: new Date()
     }
-    $scope.description = '';
-    $scope.title = '';
     $scope.tinymceOptions = {
         height: 300,
         branding: false,
         statusbar: false,
         skin: 'lightgray',
         theme: 'modern',
-        menu: {}
+        menu: {},
+        init_instance_callback: function (editor) {
+            var textContentTrigger = function () {
+                $scope.description = editor.getBody().textContent;
+                $scope.$apply();
+            };
+            editor.on('KeyUp', textContentTrigger);
+        }
     }
     $scope.submit = function (data) {
         var rand = Math.floor(Math.random() * 6) + 1;
@@ -35,5 +42,6 @@ app.controller('newIssues', function ($scope) {
         obj.status = 'Open';
         arr.push(obj);
         window.localStorage.setItem('issues',JSON.stringify(arr))
+        window.location = '#!/issuedetail/' + obj.issueNumber;
     }
 })
